3220362ce6a76b1a2ad97a9ac8565580dcfb07e4
skkim7821 <skkim7821@gmail.com>
2016-06-06 18:39:42 +0900

    16 complete

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -4,7 +4,6 @@ import { withRouter } from 'react-router';
 import * as actions from '../actions';
 import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
-import { fetchTodos } from '../api';
 
 class VisibleTodoList extends Component {
   componentDidMount() {
@@ -12,16 +11,14 @@ class VisibleTodoList extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.filter != prevProps.filter) {
+    if (this.props.filter !== prevProps.filter) {
       this.fetchData();
     }
   }
 
   fetchData() {
-    const { filter, receiveTodos } = this.props;
-    fetchTodos(filter).then(todos =>
-      receiveTodos(filter, todos)
-    );
+    const { filter, fetchTodos } = this.props;
+    fetchTodos(filter);
   }
 
   render() {

55e79fbf25ddc0af2856ffca0c7c0378c780e020
skkim7821 <skkim7821@gmail.com>
2016-06-06 16:35:27 +0900

    13 complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
 import configureStore from './configureStore';
+import { fetchTodos } from './api';
+
+fetchTodos('all').then(todos =>
+  console.log(todos)
+);
 
 const store = configureStore();
-
 render(
   <Root store={store} />,
   document.getElementById('root')
